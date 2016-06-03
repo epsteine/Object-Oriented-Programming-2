33d5b144f09681d1768aae83f90add65a205a676
elizabeth <elizabeth.e.epstein@gmail.com>
2016-06-03 15:55:29 -0400

    testing methods with ellie

--- people.rb
+++ people.rb
@@ -1,28 +1,27 @@
-# class Parent
-#   attr_accessor :name
-#
-# def walk
-#   "#{@name} is walking to school"
-# end
-#
-# parent1 = Parent.new
-# parent1 = "John"
-# p parent1.name
-# p parent1.walk
+class Person
+  # attr_accessor :name
+  def initialize(name)
+  @name = name
+  end
+  def greeting
+    "Hi, my name is #{@name}."
+  end
+end
 
 
-class Student
+class Student < Person
  def learn
    puts "I get it!"
  end
 end
 
 
-class Instructor
+class Instructor < Person
   def teach
     puts "Everything in Ruby is an Object"
   end
 end
 
-ellie = Student.new
+ellie = Student.new("Ellie")
 p ellie.learn
+p ellie.greeting
