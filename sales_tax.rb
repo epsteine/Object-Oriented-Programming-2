54bf6f336997735d88e50bfbe1ca8894b89e5ab0
elizabeth <elizabeth.e.epstein@gmail.com>
2016-06-04 13:44:12 -0400

    filling in definitions in sales tax exercise

--- sales_tax.rb
+++ sales_tax.rb
@@ -1,5 +1,31 @@
-class Item
+class Item #Cart
+  attr_accessor :name, :price, :sales_tax, :import_tax
+
+  def initialize(name, price, sales_tax = nil, import_tax = nil)
+    @name = name
+    @price = price
+    @sales_tax = sales_tax
+    @import_tax = import_tax
+  end
+
+  def sales_tax
+    @sales_tax = (0.10 * @price)
+  end
+
+  def import_tax
+    @import_tax = (0.05 * @price)
+  end
 end
 
-class Receipt
+
+class Receipt < Item
+  def total_sales_tax
+    @import_tax + @sales_tax
+  end
+
+  def total
+    @price + @import_tax + @sales_tax
+  end
 end
+
+chocolate = Item.new("chocolate", 2, true, false)
