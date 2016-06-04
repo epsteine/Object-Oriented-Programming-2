05cc207b24480b8505a7fc0766ea4455bb0d1019
elizabeth <elizabeth.e.epstein@gmail.com>
2016-06-04 16:59:04 -0400

    worked out taxes in item class

--- sales_tax.rb
+++ sales_tax.rb
@@ -8,24 +8,39 @@ class Item #Cart
     @import_tax = import_tax
   end
 
-  def sales_tax
-    @sales_tax = (0.10 * @price)
-  end
+  def taxes #accounts for all tax scenarios
 
-  def import_tax
-    @import_tax = (0.05 * @price)
-  end
-end
-
-
-class Receipt < Item
-  def total_sales_tax
-    @import_tax + @sales_tax
-  end
-
-  def total
-    @price + @import_tax + @sales_tax
+    if (@sales_tax == true) && (@import_tax == true)
+      @price * 1.15
+    elsif (@sales_tax == true) && (@import_tax == false)
+      @price * 1.1
+    elsif (@sales_tax == false) && (@import_tax == true)
+      @price * 1.05
+    elsif (@sales_tax == false) && (@import_tax == false)
+      @price
+    end
   end
 end
+#@sales_tax = (0.10 * @price)
+#   end
+#
+#   def import_tax
+#     @import_tax = (0.05 * @price)
+#   end
+#
+#   def total_sales_tax
+#     @import_tax + @sales_tax
+#   end
+#
+# end
+
+
+# class Receipt < Item
+#
+#   def total
+#     @price + @taxes
+#   end
+# end
 
 chocolate = Item.new("chocolate", 2, true, false)
+p chocolate.taxes
